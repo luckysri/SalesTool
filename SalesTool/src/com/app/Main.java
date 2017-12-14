0b7c3cc7ae98f73a2918300703be4e5e0d449d03
Srikanth Alla <Srikanth Alla@DESKTOP-S4CP2D4>
2017-12-14 18:20:07 +0530

    initial checking of sales tool

---
+++ SalesTool/src/com/app/Main.java
@@ -0,0 +1,18 @@
+package com.app;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		
+	SalesData data=new SalesData();
+	
+	displayGreeting();
+	data.display();
+	}
+
+	private static  void displayGreeting()
+	{
+		System.out.println("hello happy sales people");
+		System.out.println("hello app shoes  sales");
+	}
+}
---
+++ SalesTool/src/com/app/SalesData.java
@@ -0,0 +1,9 @@
+package com.app;
+
+public class SalesData {
+	public void display()
+	{
+		System.out.println("this is display data");
+	}
+
+}
